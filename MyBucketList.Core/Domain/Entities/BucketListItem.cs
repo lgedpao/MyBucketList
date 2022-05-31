f347b97d9785aa430f18603cb22da8c8b7e94502
Leonard Glenn T Edpao <56261683+lgedpao@users.noreply.github.com>
2022-05-31 22:52:21 +0800

    Added description property in BLI

--- MyBucketList.Core/Domain/Entities/BucketListItem.cs
+++ MyBucketList.Core/Domain/Entities/BucketListItem.cs
@@ -10,5 +10,7 @@ namespace MyBucketList.Core.Domain.Entities
         public int Id { get; set; }
 
         public string Title { get; set; }
+
+        public string Description { get; set; }
     }
 }
